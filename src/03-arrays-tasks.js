5890dcce38a5c928efd7fd497572a7deb84a0d4c
Jokernics <spirin.egor13@gmail.com>
2022-07-24 00:29:31 +0300

    feat: arrays

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -523,8 +523,17 @@ function distinct(arr) {
  *    "Poland" => ["Lodz"]
  *   }
  */
-function group(/* array, keySelector, valueSelector */) {
-  throw new Error('Not implemented');
+function group(array, keySelector, valueSelector) {
+  return array.reduce((acc, cur) => {
+    const key = keySelector(cur);
+    const value = valueSelector(cur);
+    if (acc.has(key)) {
+      const item = acc.get(key);
+      item.push(value);
+      return acc.set(key, item);
+    }
+    return acc.set(key, [value]);
+  }, new Map());
 }
 
 
@@ -541,8 +550,8 @@ function group(/* array, keySelector, valueSelector */) {
  *   [[1, 2], [3, 4], [5, 6]], (x) => x     =>   [ 1, 2, 3, 4, 5, 6 ]
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.map((el) => childrenSelector(el)).flat();
 }
 
 
@@ -558,8 +567,8 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, cur) => acc[cur], arr);
 }
 
 
@@ -581,8 +590,16 @@ function getElementByIndexes(/* arr, indexes */) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  if (arr.length > 1) {
+    let middle = arr.length / 2;
+    if (middle % 2 === 0) {
+      return [...arr.slice(middle), ...arr.slice(0, middle)];
+    }
+    middle = Math.floor(middle);
+    return [...arr.slice(middle + 1), arr[middle], ...arr.slice(0, middle)];
+  }
+  return arr;
 }
 
 
